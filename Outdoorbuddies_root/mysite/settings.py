5452058f6286182650817edc8d60919907f64927
AKashton <ashtonrcurry@hotmail.com>
2023-12-08 14:53:23 -0900

    added database

--- Outdoorbuddies_root/mysite/settings.py
+++ Outdoorbuddies_root/mysite/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'outdoor_buddies',
+        'USER': 'postgres',
+        'PASSWORD': 'Chrisowns1!',
+        'HOST': 'localhost',  # Set to empty string for localhost.
+        'PORT': '5432',       # Default port for PostgreSQL.
     }
 }
+
 
 
 # Password validation
