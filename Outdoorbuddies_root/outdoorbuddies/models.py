16de2d9396d3b2407e435a386486edadd5fe19f3
Ashtonrcurry <ashtonrcurry@hotmail.com>
2023-12-09 14:43:10 -0900

    added user settings, password stuff

--- Outdoorbuddies_root/outdoorbuddies/models.py
+++ Outdoorbuddies_root/outdoorbuddies/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+#tags include biking and hiking
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
+#this model is the bassi for creating posts for adventures
 class Adventure(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     picture = models.ImageField(upload_to='adventure_pics')
@@ -20,6 +24,7 @@ class Adventure(models.Model):
     def __str__(self):
         return self.description
 
+# comments on adventures
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     adventure = models.ForeignKey(Adventure, on_delete=models.CASCADE)
@@ -29,15 +34,18 @@ class Comment(models.Model):
     def __str__(self):
         return self.text
 
+#users can like adventures
 class UserLikes(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     adventure = models.ForeignKey(Adventure, on_delete=models.CASCADE)
 
+#model to save participants
 class AdventureParticipants(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     adventure = models.ForeignKey(Adventure, on_delete=models.CASCADE)
     status = models.CharField(max_length=100)
 
+#profile setup
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_photo = models.ImageField(upload_to='profile_photos', blank=True)
@@ -46,3 +54,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+#signals to instance a new profile for a new user
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+#saves the profile instance
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
