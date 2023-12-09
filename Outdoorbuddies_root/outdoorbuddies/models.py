d47e6454dc42cc32d05e6ff1cbb40b0020d30ec5
AKashton <ashtonrcurry@hotmail.com>
2023-12-08 17:55:29 -0900

    added models to project

--- Outdoorbuddies_root/outdoorbuddies/models.py
+++ Outdoorbuddies_root/outdoorbuddies/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Adventure(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='adventure_pics')
+    description = models.TextField()
+    max_participants = models.IntegerField()
+    location = models.CharField(max_length=255)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    event_datetime = models.DateTimeField()
+    status = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.description
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    adventure = models.ForeignKey(Adventure, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+class UserLikes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    adventure = models.ForeignKey(Adventure, on_delete=models.CASCADE)
+
+class AdventureParticipants(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    adventure = models.ForeignKey(Adventure, on_delete=models.CASCADE)
+    status = models.CharField(max_length=100)
