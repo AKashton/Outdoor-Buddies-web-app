a865018b071c070d21432befaf11624b6e853abf
AKashton <ashtonrcurry@hotmail.com>
2023-12-09 19:03:35 -0900

    small file changes

--- Outdoorbuddies_root/mysite/settings.py
+++ Outdoorbuddies_root/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os #for mediafiles
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,7 +130,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
